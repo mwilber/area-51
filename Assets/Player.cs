2f26a6e0bf84fdcd94c0676088557d4cf587b838
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2018-10-17 23:54:53 -0400

    Ship controls - Attach ship to camera - Add cross platform control interface - Add skybox

---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")] [SerializeField] float xRange = 10f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+
+        float rawNewXPos = transform.localPosition.x + xOffset;
+
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+		
+	}
+}
